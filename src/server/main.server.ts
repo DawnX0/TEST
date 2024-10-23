58684a82e8f6626cecec110019fc7b45606bb5f4
Seph <josephshanklin3@outlook.com>
2024-10-22 21:12:29 -0400

    commit

--- src/server/main.server.ts
+++ src/server/main.server.ts
@@ -3,27 +3,18 @@ import SimpleInput from "@rbxts/simpleinput";
 import SimpleTimer from "@rbxts/simpletimer";
 import DCS from "@rbxts/dcs";
 
-const Server = SimpleInput.Server;
-Server.Listen();
+const DeathTimer = SimpleTimer.CreateTimer("DeathTimer", 5, 1, true);
 
-const DefaultBinds = new Map<Enum.KeyCode | Enum.UserInputType, string>([
-	[Enum.KeyCode.Q, "Dash"],
-	[Enum.KeyCode.E, "Equip"],
-]);
-
-const Responses = new Map<string, () => void>([
-	["Dash", () => print("Dash server")],
-	["Equip", () => print("Server Equiping!")],
-]);
-
-Server.SetDefaultBinds(DefaultBinds);
-Server.SetServerResponses(Responses);
-
-Players.PlayerAdded.Connect((player: Player) => {
-	print(`${player} has joined.`);
+const DashSkill = DCS.CreateSkill({
+	Name: "Dash",
+	CooldownTime: 0.5,
+	CastTime: 1,
+	Cast: (entries) => {
+		print(entries.Name);
+	},
 });
 
-const DeathTimer = SimpleTimer.CreateTimer("DeathTimer", 5, 1, true);
+DCS.RegisterSkill(DashSkill);
 
 const BurnEffect = DCS.CreateStatusEffect("Burn", 5, 1, (entries) => {
 	const Model = entries.Model;
@@ -35,25 +26,42 @@ const BurnEffect = DCS.CreateStatusEffect("Burn", 5, 1, (entries) => {
 
 DCS.AddStatusEffect(BurnEffect);
 
-const DashSkill = DCS.CreateSkill("Dash", (entries) => {
-	const Model = entries.Model;
-	const RootPart = Model.FindFirstChild("HumanoidRootPart") as BasePart;
+const DefaultBinds = new Map<Enum.KeyCode | Enum.UserInputType, string>([
+	[Enum.KeyCode.Q, "Dash"],
+	[Enum.KeyCode.E, "Equip"],
+]);
 
-	if (RootPart) {
-		RootPart.AssemblyLinearVelocity = RootPart.CFrame.LookVector.mul(40);
-	}
-});
+const Responses = new Map<string, (player: Player) => void>([
+	[
+		"Dash",
+		(player) => {
+			const character = player.Character;
+			if (character) {
+				const actor = DCS.GetActor(character);
+				if (actor) {
+					actor.CastSkill("Dash");
+				}
+			}
+		},
+	],
+	["Equip", () => print("Server Equiping!")],
+]);
 
-DCS.RegisterSkill(DashSkill);
+const Server = SimpleInput.Server;
+Server.Listen();
+
+Server.SetDefaultBinds(DefaultBinds);
+Server.SetServerResponses(Responses);
 
 Players.PlayerAdded.Connect((player: Player) => {
+	print(`${player} has joined.`);
 	player.CharacterAdded.Connect((character: Model) => {
 		DCS.AddActor(character);
 
 		const actor = DCS.GetActor(character);
 		if (actor) {
 			actor.ApplyStatusEffect("Burn");
-			task.wait(3);
+			task.wait(1);
 			actor.RemoveStatusEffect("Burn");
 		}
 	});
